9df0a4608d59c57ffbafdb54f928ed161bbdf299
WatchJani <jankokondic84@gmail.com>
2021-01-13 20:50:07 +0100

    app

--- src/components/newmesseg.js
+++ src/components/newmesseg.js
@@ -4,34 +4,38 @@ import React, { Component } from "react"
 class Enter extends Component {
 
     state = {
-        mess : ""
+        mess: ""
     }
 
-    nova_poruka = e =>{
+    nova_poruka = e => {
         this.setState(
-            {mess : e.target.value}
+            { mess: e.target.value }
         )
     }
 
 
-    poruka = () =>{
-       this.props.dodajporuku(this.state)
-       this.setState({mess : ""})
+    poruka = (e) => {
+        if (e.target.value != "") {
+            this.props.dodajporuku(this.state)
+            this.setState({ mess: "" })
+        }
     }
 
-
-   dole= e =>{
-        if(e.keyCode===13){
-            this.props.dodajporuku(this.state)
-            this.setState({mess : ""})
+    /*dodaj poruku na enter*/
+    dole = e => {
+        if (e.keyCode === 13) {
+            if (e.target.value != "") {
+                this.props.dodajporuku(this.state)
+                this.setState({ mess: "" })
+            }
         }
-   }
+    }
 
 
     render() {
         return (
             <div className="nesto">
-                <input type="text" name="" id="enter" placeholder="Vaša poruka" onChange={this.nova_poruka} onKeyUp={this.dole} value={this.state.mess}/>
+                <input type="text" name="" id="enter" placeholder="Vaša poruka" onChange={this.nova_poruka} onKeyUp={this.dole} value={this.state.mess} />
                 <button onClick={this.poruka}>Pošalji</button>
             </div>
         )
